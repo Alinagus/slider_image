baefb8978496cbe326f2fa050973bf3bb16e99df
Alinagus <124495073+Alinagus@users.noreply.github.com>
2024-03-26 13:33:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 const images = [
-    {
+  {
     text: 'Rostov-on-Don LCD admiral',
     time: '3.5 months',
     area: '81 m2',
     url: "img/image-1.svg"
-},
-{
+  },
+  {
     text: 'Sochi Thieves',
     time: '4 months',
     area: '105 m2',
     url: 'img/image-2.png'
-},
-{
+  },
+  {
     text: 'Rostov-on-Don Patriotic',
     time: '3 months',
     area: '93 m2',
     url: 'img/image-3.png'
-}
-]
+  }
+  ]
+
 
 function initSlider(options) {
   if (!images || !images.length) return;
-    
+  
+
   
   let sliderImages = document.querySelector(".box__image_slider");
   let sliderArrows = document.querySelectorAll(".slider_arrow");
   let sliderDots = document.querySelector('.slider_dots')
   const text = document.querySelector(".city");
   const time = document.querySelector(".time");
-  const area = document.querySelector(".area")
+  const area = document.querySelector(".area");
+  let city = document.querySelectorAll('.name_city')
 
-  
+
+
   initImages();
   initArrows();
+  initTitles();
 
   if (options.dots) {
     initDots();
@@ -43,14 +48,14 @@ function initSlider(options) {
   
   
   
-function initImages() {
-    images.forEach((image, index) => {
-      let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image: url(${images[index].url});" data-index="${index}"></div>`;
-      sliderImages.innerHTML += imageDiv;
-    });
+  function initImages() {
+      images.forEach((image, index) => {
+        let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image: url(${images[index].url});" data-index="${index}"></div>`;
+        sliderImages.innerHTML += imageDiv;
+      });
   }
   
-function initArrows() {
+  function initArrows() {
     sliderArrows.forEach(arrow => {
       arrow.addEventListener("click", function() {
         let curNumber = +sliderImages.querySelector(".active").dataset.index;
@@ -62,44 +67,54 @@ function initArrows() {
           nextNumber = curNumber === images.length - 1? 0 : curNumber + 1;
         }
         NextText(nextNumber);
-        slideText(nextNumber);
         moveSlider(nextNumber);
       });
     });
   }
 
-  
-
   function initDots() {
     images.forEach((image, index) => {
       let dot = `<div class="slider__dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`
     sliderDots.innerHTML += dot;
-  });
-  sliderDots.querySelectorAll(".slider__dots-item").forEach(dot => {
-    dot.addEventListener("click", function() {
+    });
 
-      ind = this.dataset.index;
-      NextText(ind)
-      slideText(ind)
-      moveSlider(ind)
+    sliderDots.querySelectorAll(".slider__dots-item").forEach(dot => {
+      dot.addEventListener("click", function() {
+        ind = this.dataset.index;
+        NextText(ind)
+        moveSlider(ind)
+      })
     })
-  })
-}
-
-function slideText(index){
-      sliderDots.querySelector(".active")
-      changeText = document.querySelector(`.num${index}`);
-      changeText.className += ' active';
-
   }
 
-function NextText(num){
-  text.innerText = images[num].text;
-  time.innerText = images[num].time;
-  area.innerText = images[num].area;
-}
+
+  function NextText(num){
+    text.innerText = images[num].text;
+    time.innerText = images[num].time;
+    area.innerText = images[num].area;
+  }
+
+  function initTitles(){
+    city.forEach(i => {
+      i.addEventListener("click", function(){
+        if (i.classList.contains('num0' )){
+          num = 0;
+        }
+        else if(i.classList.contains('num1')){
+          num = 1;
+        }
+        else{
+          num = 2;
+        }
+        moveSlider(num);
+        NextText(num)
+      })
+    })
+  }
 
   function moveSlider(num) {
+    document.querySelector(".active1").classList.remove("active1");
+    document.querySelector(".num" + num).classList.add("active1");
     sliderImages.querySelector(".active").classList.remove("active");
     sliderImages.querySelector(".n" + num).classList.add("active");
 
@@ -110,15 +125,13 @@ function NextText(num){
 
     }
   }
+
 }
 
 let sliderOptions = {
   dots: true,
-
 };
 
 document.addEventListener("DOMContentLoaded", function() {
   initSlider(sliderOptions);
 });
-
-
